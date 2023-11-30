38e9a7d7570e708b473c1ce70096f415b53f5078
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2023-11-30 09:31:38 -0500

    Added smooth scrolling. Added spying on sidebar icons.

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,45 +1,77 @@
 import { faGithub, faMastodon, faReddit } from "@fortawesome/free-brands-svg-icons";
 import { faHome, faUser, faEnvelope, faCode, faUserGraduate } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 import "./Sidebar.scss";
 
 function Sidebar() {
-
+	
+	const scroll_options = {
+		duration: 1000,
+		smooth: true,
+	}; 
+	
+	const scrollToTop = () => {
+		scroll.scrollToTop(scroll_options);
+	};
 
 	return (
 		<div className="nav-bar">
 			
-			<a href="#top" className="logo">XB</a>
+			<a href="#" onClick={scrollToTop} className="logo">XB</a>
 
 			<nav>
-				<a href="#greeter">
+				<Link
+					activeClass=""
+					to="greeter"
+					spy={true}
+					smooth={true}
+					duration={500}
+
+				>
 					<FontAwesomeIcon icon={faHome} />
-				</a>
-				<a 
-					href="#about"
+				</Link>
+				<Link
 					className="about-link"
+					activeClass=""
+					to="about"
+					spy={true}
+					smooth={true}
+					duration={500}
 				>
 					<FontAwesomeIcon icon={faUser} />
-				</a>
-				<a 
-					href="#skills"
+				</Link>
+				<Link
 					className="skills-link"
+					activeClass=""
+					to="skills"
+					spy={true}
+					smooth={true}
+					duration={500}
 				>
 					<FontAwesomeIcon icon={faUserGraduate} />
-				</a>
-				<a 
-					href="#projects"
+				</Link>
+				<Link
 					className="projects-link"
+					activeClass=""
+					to="projects"
+					spy={true}
+					smooth={true}
+					duration={500}
 				>
 					<FontAwesomeIcon icon={faCode} />
-				</a>
-				<a 
-					href="#contact"
+				</Link>
+				<Link
 					className="contact-link"
+					activeClass=""
+					to="contact"
+					spy={true}
+					smooth={true}
+					duration={500}
 				>
 					<FontAwesomeIcon icon={faEnvelope} />
-				</a>
+				</Link>
 			</nav>
 			<ul>
 				<li><a target="_blank" rel="noreferrer" href="https://github.com/xavier-berthiaume">
